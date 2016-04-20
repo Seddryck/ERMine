9ca3a9b0790bf0d7f88d1b6ac1996557efbcf722
Cédric L. Charlier <cedric.charlier@yucom.be>
2016-04-20 22:58:14 +0200

    Added AssemblyInformationalVersion

--- ERMine.Core/Properties/AssemblyInfo.cs
+++ ERMine.Core/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -31,9 +31,10 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyVersion("1.0.0.5")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-RC-1.5+Branch.master.Sha.1c321f178849611c7a521e04186ae5a86a27c7ec")]
 
 //Testing
 [assembly: InternalsVisibleTo("ERMine.UnitTesting.Core")]
